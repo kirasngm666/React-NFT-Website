3d15d41028c89efb3a3afe9b3c798e1a140f345d
Duc Nguyen <Nguyenducquocanh@gmail.com>
2022-04-17 11:06:47 -0700

    Stop at 13:18

---
+++ src/routes/Routers.js
@@ -0,0 +1,16 @@
+
+
+import React from 'react'
+import Home from '../pages/Home'
+import Market from '../pages/Market'
+import Create from '../pages/Create'
+import Contact from '../pages/Contact'
+import SellerProfile  from '../pages/SellerProfile'
+
+const Routers = () => {
+  return (
+    <div>Routers</div>
+  )
+}
+
+export default Routers
